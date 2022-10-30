d1866a97066144849dc5f3b280bd669bf73f088b
Ioana Simtion <ionitza6@gmail.com>
2022-10-30 16:18:32 +0200

    added text file

--- variante-recapitulare/functii.h
+++ variante-recapitulare/functii.h
@@ -1,7 +1,36 @@
 using namespace std;
 #include <iostream>
+#include <fstream>
 #pragma once
 
 int numarare(int n, int m, int a[], int b[]) {
-
+	int ct = 0;
+	bool flag = 1;
+	for (int i = 0; i < n; i++) {
+		flag = 1;
+		for (int j = 0; j < m  && flag; j++) {
+			if (a[i] >= b[j]) {
+				flag = 0;
+			}
+		}
+		if (flag) {
+			ct++;
+		}
+	}
+	return ct;
 }
+
+//todo functie ce citeste un vector 
+
+void citire(int v[], int& d) {
+	d = 0;
+	ifstream f ("citire.txt");
+	while (!f.eof()) {
+		f >> v[d];
+		d++;
+	}
+}
+
+//todo functie ce afiseaza un vector
+
+void afisare(int v[], int d)
--- variante-recapitulare/solutii.h
+++ variante-recapitulare/solutii.h
@@ -11,6 +11,14 @@ using namespace std;
 //ex2:
 
 void sol1() {
-	int n = 7, m = 8, a[100] = { 1,4,5,3,82,6,2 }, b[100];
+	int n = 7, m = 8, a[100] = { 1,4,5,3,82,6,2 }, b[100]={56,6,34,23,8,9,12,18};
+	cout << numarare(n, m, a, b);
+}
 
+//ex3:
+
+void sol2() {
+	int d, v[100];
+	citire(v, d);
+	
 }
